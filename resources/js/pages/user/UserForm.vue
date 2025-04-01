be05f29ee846438450897ef5b61c456f8e17e06c
Victor Emeka Mbachu <victor.c.mbachu@gmail.com>
2025-04-01 10:20:18 +0100

    working on frontend form submittion validation

--- resources/js/pages/user/UserForm.vue
+++ resources/js/pages/user/UserForm.vue
@@ -87,9 +87,8 @@ const validatePhone = (event) => {
   }
 }
 
-const validateEmailConfirmation = (event, first_value, second_value, first_key, second_key) => {
-
-  let identical = formValidations.emailConfirmation(first_value, second_value);
+const validatePassword = (event, password, password_confirm) => {
+  let identical = formValidations.passwordConfirmation(password, password_confirm);
   if(!identical){
     errors.value['password'] = ['Password and confirmation do not match'];
     return false;
@@ -118,10 +117,15 @@ onBeforeMount(() => {
       </div>
 
       <form @submit.prevent="submitForm" class="max-w-sm mx-auto">
+
             <div class="mb-5">
                 <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
                 <input v-model="form.name" type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
+                <p v-if="errors.name" class="text-rose-300">
+                  {{ errors.name[0] }}
+                </p>
             </div>
+
             <div class="mb-5">
                 <label for="surname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Surname</label>
                 <input
@@ -130,15 +134,27 @@ onBeforeMount(() => {
                     id="surname"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="" required />
+                <p v-if="errors.surname" class="text-rose-300">
+                  {{ errors.surname[0] }}
+                </p>
             </div>
+
             <div class="mb-5">
                 <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
                 <input v-model="form.email" type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+              <p v-if="errors.email" class="text-rose-300">
+                {{ errors.email[0] }}
+              </p>
             </div>
+
             <div class="mb-5">
                 <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone</label>
-                <input v-model="form.phone" type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+                <input v-model="form.phone" @change="validatePhone($event)" type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+                <p v-if="errors.phone" class="text-rose-300">
+                  {{ errors.phone[0] }}
+                </p>
             </div>
+
             <div class="mb-5">
                 <label for="gender" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Gender</label>
                 <select id="gender"
@@ -149,7 +165,11 @@ onBeforeMount(() => {
                     <option value="male">Male</option>
                     <option value="female">Female</option>
                 </select>
+              <p v-if="errors.gender" class="text-rose-300">
+                {{ errors.gender[0] }}
+              </p>
             </div>
+
             <div class="mb-5">
                 <label for="country" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Country</label>
                 <select id="country" v-model="form.country" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -159,11 +179,23 @@ onBeforeMount(() => {
                         {{ country.name }}
                     </option>
                 </select>
+              <p v-if="errors.country" class="text-rose-300">
+                {{ errors.country[0] }}
+              </p>
             </div>
+
             <div class="mb-5">
                 <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-                <input v-model="form.password" type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                <input
+                    v-model="form.password"
+                    @change="validatePassword($event, form.password, form.password_confirmation)"
+                    type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+              <p v-if="errors.password"
+                 class="text-rose-300 font-bold">
+                {{ errors.password[0] }}
+              </p>
             </div>
+
             <div class="mb-5">
                 <label for="password_confirmation" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
                 <input v-model="form.password_confirmation" type="password" id="password_confirmation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
@@ -175,7 +207,6 @@ onBeforeMount(() => {
                     accept="image/*"
                     @change="uploadSelfie($event)"
                     class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="user_avatar_help" id="user_avatar" type="file"
-                    required
                 >
 
               <div v-if="form.selfie !== null && !errors?.selfie" class="flex">
@@ -187,10 +218,22 @@ onBeforeMount(() => {
                 </span>
               </div>
 
-              <span v-if="errors.image"
-                    class="text-danger">
-                  {{ errors.image[0] }}
-              </span>
+              <p v-if="errors.selfie"
+                    class="text-rose-300 font-bold">
+                  {{ errors.selfie[0] }}
+              </p>
+            </div>
+
+            <div class="mb-5">
+              <label
+                  for="introduction"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Introduction</label>
+              <textarea
+                  v-model="form.introduction"
+                  id="introduction"
+                  rows="4"
+                  class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Write your thoughts here..."></textarea>
             </div>
 
             <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
